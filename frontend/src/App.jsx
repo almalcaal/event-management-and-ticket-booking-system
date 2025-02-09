cee0b1fe6082229f15b84e8067d8702b9f5cf11c
Aldrin Caalim <aldrinmcaalim@gmail.com>
2025-02-09 16:14:00 -0500

    frontend - set up header/footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
 
+import { Container } from "react-bootstrap";
+import Header from "./components/common/Header.component";
+import Footer from "./components/common/Footer.component";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Ticket Tracker</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>FakeEvents</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
