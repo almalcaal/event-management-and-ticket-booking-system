53b09a7788d9fc6d767c495810d6eeaf9664580f
Aldrin Caalim <aldrinmcaalim@gmail.com>
2025-02-17 23:27:56 -0500

    frontend - setup/create redux store & api slice

---
+++ frontend/src/constants.js
@@ -0,0 +1,6 @@
+// unnecessary due to our use of a proxy, it will use the same domain
+// export const BASE_URL =
+//   process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
+
+export const BASE_URL = "";
+export const ACTIVITIES_URL = "/api/activities";
