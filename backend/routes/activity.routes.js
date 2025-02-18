f27e7e1ebd72309755787b130b9a4601e97dbb53
Aldrin Caalim <aldrinmcaalim@gmail.com>
2025-02-17 19:40:08 -0500

    backend - activity controller

--- backend/routes/activity.routes.js
+++ backend/routes/activity.routes.js
@@ -1,29 +1,11 @@
 import express from "express";
 const router = express.Router();
-import asyncHandler from "../middleware/asyncHandler.js";
-import Activity from "../models/activity.model.js";
-// import activities from "../data/activities.js";
+import {
+  getActivities,
+  getActivityById,
+} from "../controllers/activity.controller.js";
 
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const activities = await Activity.find({});
-    res.json(activities);
-  })
-);
-
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const activity = await Activity.findById(req.params.id);
-
-    if (activity) {
-      res.json(activity);
-    } else {
-      res.status(404);
-      throw new Error("Resource not found");
-    }
-  })
-);
+router.route("/").get(getActivities);
+router.route("/:id").get(getActivityById);
 
 export default router;
