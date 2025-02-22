cad8fa5a153f1f5eb99e68a64a99181f3884f95c
Aldrin Caalim <aldrinmcaalim@gmail.com>
2025-02-22 16:45:06 -0500

    backend - upload activity image

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import path from "path";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/user.routes.js";
 import activityRoutes from "./routes/activity.routes.js";
@@ -20,9 +21,17 @@ app.use(express.urlencoded({ extended: true }));
 // cookie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.use("/api/users", userRoutes);
 app.use("/api/activities", activityRoutes);
@@ -31,6 +40,9 @@ app.use("/api/orders", orderRoutes);
 app.get("/api/config/paypal", (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
 app.use(errorHandler);
