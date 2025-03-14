65d4725f1a6410e4e0c78dd8f39791ac5c48a876
Aldrin Caalim <aldrinmcaalim@gmail.com>
2025-03-14 13:15:00 -0400

    changed file path

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
