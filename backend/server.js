2214aaf91e6268049a087f1b44272166041c016c
Aldrin Caalim <aldrinmcaalim@gmail.com>
2025-03-14 13:25:03 -0400

    had to adjust server

--- backend/server.js
+++ backend/server.js
@@ -25,18 +25,6 @@ app.use(express.urlencoded({ extended: true }));
 // cookie parser middleware
 app.use(cookieParser());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running....");
-  });
-}
-
 app.use("/api/users", userRoutes);
 app.use("/api/activities", activityRoutes);
 app.use("/api/orders", orderRoutes);
